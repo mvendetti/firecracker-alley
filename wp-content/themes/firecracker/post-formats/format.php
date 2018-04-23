f75e12a01e86be43e6dc28bc210c41501382181b
Mike Vendetti <mike@tacomaweekly.com>
2018-04-23 14:44:16 -0700

    Removed comments and author from displaying

--- wp-content/themes/firecracker/post-formats/format.php
+++ wp-content/themes/firecracker/post-formats/format.php
@@ -25,8 +25,8 @@
                     <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
                        /* the time the post was published */
                        '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       /* the author of the post */
-                       '<span class="by">'.__( 'by', 'bonestheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                       /* empty argument for now because I suck at PHP */
+                       ''
                     ); ?>
 
                   </p>
@@ -58,14 +58,6 @@
                     ) );
                   ?>
                 </section> <?php // end article section ?>
-
-                <footer class="article-footer">
-
-                  <?php printf( __( 'filed under', 'bonestheme' ).': %1$s', get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-                </footer> <?php // end article footer ?>
 
                 <?php //comments_template(); ?>
 
