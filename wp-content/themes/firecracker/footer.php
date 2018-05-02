54c30dcc298e157a5e1ab8a7bb6aee966002e045
Mike Vendetti <mike@tacomaweekly.com>
2018-05-02 11:47:50 -0700

    Mobile menu needs bootstrap js

--- wp-content/themes/firecracker/footer.php
+++ wp-content/themes/firecracker/footer.php
@@ -28,7 +28,7 @@
 
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
-
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 	</body>
 
 </html> <!-- end of site. what a ride! -->
