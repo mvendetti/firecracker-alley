8480018b252df4e73c77caef38798076ad16d5dd
Mike Vendetti <mike@tacomaweekly.com>
2018-05-03 11:59:04 -0700

    favicon and head cleaning

--- wp-content/themes/firecracker/header.php
+++ wp-content/themes/firecracker/header.php
@@ -7,40 +7,22 @@
 
 <head>
     <meta charset="utf-8">
-
-    <?php // force Internet Explorer to use the latest rendering engine available ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <title><?php wp_title(''); ?></title>
 
-    <?php // mobile meta (hooray!) ?>
     <meta name="HandheldFriendly" content="True">
     <meta name="MobileOptimized" content="320">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
-        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-        <!--[if IE]>
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-        <![endif]-->
-        <?php // or, set /favicon.ico for IE10 win ?>
-        <meta name="msapplication-TileColor" content="#DE5454">
-        <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
-        <meta name="theme-color" content="#121212">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/library/images/favicon.png">
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
-        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-        <?php // wordpress head functions ?>
-        <?php wp_head(); ?>
-        <?php // end of wordpress head ?>
-
-        <?php // drop Google Analytics Here ?>
-        <?php // end analytics ?>
-
-    </head>
+    <?php wp_head(); ?>
+</head>
 
     <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
         <div id="container">
