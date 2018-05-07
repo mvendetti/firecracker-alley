78d16b3286c3f54effdbf0ff9f09d80f6c3b1aba
Mike Vendetti <mike@tacomaweekly.com>
2018-05-07 12:19:08 -0700

    Gallery page

---
+++ wp-content/themes/firecracker/gallery.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: Gallery */ ?>
+
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Photo Gallery</h1>
+            <?php
+                // I think this is a stupid thing to do but #YOLO
+                $id = 90;
+                $post = get_post($id);
+                $content = apply_filters('the_content', $post->post_content);
+                echo $content;
+            ?>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php get_footer(); ?>
